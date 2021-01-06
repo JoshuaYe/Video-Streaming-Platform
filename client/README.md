4f6aaf4c1cf2dc241e2da62fb79a6866da8fbd76
JoshuaYe <joshua.ye@live.com>
2021-01-06 18:05:57 -0500

    solid foundation code for crud app

---
+++ client/README.md
@@ -0,0 +1,5 @@
+# Free Image Generator
+
+A React-based web streaming platform where content-creators can live-stream, and viewers can browse streams then tune in! 
+
+This is a project I created for Stephen Grider's React and Redux 2020 course on Udemy.
---
+++ client/src/components/streams/StreamDelete.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Modal from "../Modal";
+import history from "../../history";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchStream, deleteStream } from "../../actions";
+
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    const { id } = this.props.match.params;
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    } else {
+      return (
+        <React.Fragment>
+          Are you sure you want to delete
+          <b>{` ${this.props.stream.title}`}</b>?
+        </React.Fragment>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <Modal
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push("/")}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
